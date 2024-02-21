7e78ea5731fdc7767f9a08362e929b23fa30ad22
CarlosArgueta <carguetar@unah.hn>
2024-02-21 10:26:22 -0600

    Tarea de Lenguajes #2

---
+++ apptipogoogle/lib/src/module/principal/pages/data_priv.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DataPrivPage extends StatelessWidget {
+  const DataPrivPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Espacio para los Datos y Privacidad'));
+  }
+}
---
+++ apptipogoogle/lib/src/module/principal/pages/info_personal.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfoPersonalPage extends StatelessWidget {
+  const InfoPersonalPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Información Personal :D'));
+  }
+}
